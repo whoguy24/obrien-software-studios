3ef1bfa86045c1ed0b58562a539ca9238cf8dc33
whoguy24 <whoguy24@gmail.com>
2022-04-01 15:03:10 -0500

    Created footer

--- obrien-software-studios/src/components/Navigation/Navigation.jsx
+++ obrien-software-studios/src/components/Navigation/Navigation.jsx
@@ -21,7 +21,7 @@ function Navigation() {
     return (
         <div className="navigation-header">
 
-            <div className="navigation-links">
+            <div className="navigation-links-left">
                 <Button variant="text" onClick={()=>navigate('/skills')}>Skills</Button>
                 <Button variant="text" onClick={()=>navigate('/education')}>Education</Button>
                 <Button variant="text" onClick={()=>navigate('/projects')}>Projects</Button>
@@ -29,7 +29,7 @@ function Navigation() {
 
             <img src="logo_white.png" alt="logo" onClick={()=>navigate('/home')}/>
 
-            <div className="navigation-links">
+            <div className="navigation-links-right">
                 <Button variant="text" onClick={()=>navigate('/about')}>About</Button>
                 <Button variant="text" onClick={()=>navigate('/faq')}>FAQ</Button>
                 <Button variant="text" onClick={()=>navigate('/contact')}>Contact</Button>
