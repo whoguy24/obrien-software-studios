9129f4976112b8509cb4b510506d5ca10f832f55
whoguy24 <whoguy24@gmail.com>
2022-09-05 11:09:21 -0500

    Implemented home page animation

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -14,6 +14,21 @@ function Home() {
     // Render DOM
     return (
         <div id="home">
+            <div id="home-container">
+
+                <div id="home-container-interior">
+
+                    <img id="home-desktop" src="/images/desktop.png"/>
+
+                    <img id="home-laptop" src="/images/laptop.png"/>
+
+                    <img id="home-tablet" src="/images/tablet.png"/>
+
+                    <img id="home-mobile" src="/images/mobile.png"/>
+
+                </div>
+
+            </div>
         </div>
     );
 }
